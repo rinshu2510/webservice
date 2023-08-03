9a0441108eb849b1bbfdd7ece6ca6cc8d236e91d
Rinshu Kumar singh <rinshu2510@gmail.com>
2023-08-04 04:06:17 +0530

    initial commit

---
+++ HotelBookingAPI/Data/ApiContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI.Models;
+using System.Collections.Generic;
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+
+        public DbSet<HotelBooking> Bookings { get; set; }
+
+        public ApiContext(DbContextOptions<ApiContext> options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
